d920b0c1809e1a079dd8f448687a12fad11b5455
prabhu1819 <amnurjeet1804@gmail.com>
2022-08-10 19:12:14 +0530

    changes

--- hello.java
+++ hello.java
@@ -1,16 +1,17 @@
 
 package selenium_April_22;
 
-public class hello {
+public class hello 
+{
+ 	public static void main(String[] args) 
+     {
 
-	public static void main(String[] args) {
-
-		System.out.println("Hello Java");
-		System.out.println("Good Morning");
-    System.out.println("Hello html");
-
-
-	}
+	System.out.println("Hello Java");
+	System.out.println("Good Morning");
+        System.out.println("Hello html");
+	System.out.println("Merge Conflict");
+	
+     }
 
 }
 
